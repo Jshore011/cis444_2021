ade024d44fa23341550fe27da5e0dbe25c8a000f
Ubuntu <ubuntu@ip-172-31-86-123.ec2.internal>
2021-11-18 02:40:33 +0000

    successfully loggin in user. working on returning books

--- assignment4/flask_jwt_rest_server/secure_calls/get_books.py
+++ assignment4/flask_jwt_rest_server/secure_calls/get_books.py
@@ -7,5 +7,26 @@ from tools.logging import logger
 def handle_request():
     logger.debug("Get Books Handle Request")
 
+
+    cur = g.db.cursor();
+    try:
+        cur.execute("select * from books;")
+        booklist=cur.fetchall()
+        cur.close()
+    except:
+        logger.debug("cannot read from database")
+        return json_response(data={"message": "Error occured while reading from database."}, status=500)
+
+    count=0
+    books = '{"books":['
+    for b in booklist:
+        if b[0] < len(booklist) :
+            books += '{"title":"'+str(b[1]) + '","price":"' + str(b[2]) + '"},'
+        else:
+            books += '{"title":"'+str(b[1]) + '","price":"' + str(b[2]) + '"}'
+    books += "]}"
+    #print(message)
+    print("sending silly token")
+    
     return json_response( token = create_token(  g.jwt_data ) , books = {})
 

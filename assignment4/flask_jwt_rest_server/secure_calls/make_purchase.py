ceb5e3c6706a03e30df09ce6c8e5c247d0a2ff31
Ubuntu <ubuntu@ip-172-31-86-123.ec2.internal>
2021-11-21 18:24:42 +0000

    able to buy books

--- assignment4/flask_jwt_rest_server/secure_calls/make_purchase.py
+++ assignment4/flask_jwt_rest_server/secure_calls/make_purchase.py
@@ -12,14 +12,14 @@ def handle_request():
     logger.debug(request.args.get('title'))
     
     try:
-        title = request.form["title"]
+        title = request.args.get('title')
         print(title)
         cur.execute("INSERT INTO purchases (title, price) SELECT title, price FROM books WHERE title = '" + title + "';")
-        cur.commit()
         cur.close()
-
+        g.db.commit();
+        
         logger.debug("Added book to cart.")
-        return json_response(data={"message" : "Succesfully added book to purchases."}, status = 200)
+        return json_response(token = create_token( g.jwt_data), data =("success"))
 
     except:
         return json_response(data={"message": "Error occured while reading from database."}, status=500)
